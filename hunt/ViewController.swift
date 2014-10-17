1393f43c47755d4235ccec24ab9c61da8233893e
Xiangying Sun <xiangyingsun@pal-nat184-070-206.itap.purdue.edu>
2014-10-16 20:21:11 -0400

    Initial Commit

---
+++ hunt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hunt
+//
+//  Created by Xiangying Sun on 10/16/14.
+//  Copyright (c) 2014 Xiangying Sun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
